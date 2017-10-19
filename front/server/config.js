3ad9fbb27590481292f00a655871f82e7eb06f2c
pini <pini.h@dynamicyield.com>
2017-10-19 17:32:21 +0300

    modify

--- front/server/config.js
+++ front/server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: process.env.MYSQL_HOST || '192.168.99.100',
-  port: process.env.MYSQL_PORT || 30158,
+  port: process.env.MYSQL_PORT || 31228,
   user: process.env.MYSQL_ROOT_PASSWORD || 'root',
   password: process.env.MYSQL_ROOT_USER || 'root',
   db: process.env.MYSQL_DATABASE || 'dev'
