9dc59ee24cb091a0e414102383f4163e2705928b
pini <pini.h@dynamicyield.com>
2017-10-26 12:04:16 +0300

    backend tests

--- front/index.js
+++ front/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const request = require('request-promise-native');
 const app = express();
+
 function ensureSec(req, res, next) {  
   if (req.headers['x-forwarded-proto'] == 'http') {
     console.log('Redirecting to https', `https://${req.hostname}${req.url}`);
@@ -11,7 +12,10 @@ function ensureSec(req, res, next) {
 app.enable('trust proxy');
 app.use(ensureSec);
 app.use(express.static('dist'));
-
+app.get('/backend-test', async (req, res) => {
+  const api = await request.get('http://backend:8080/api')
+  res.send(api);
+});
 app.all('*', function(req,res) {
   res.send('Hello World');
 });
